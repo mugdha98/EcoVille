44bd3959d9f3585d94cb8b6d73c506a77c22ee09
mugdha98 <agarwalmugdha1998@gmail.com>
2021-05-12 19:48:22 +0530

    Sign Up Seller Updated

--- client/src/components/signup-seller/SignupSeller.js
+++ client/src/components/signup-seller/SignupSeller.js
@@ -68,173 +68,167 @@ const Register = ({ setAlert, registerSeller, isAuthenticated }) => {
   }
 
   return (
-    <div className="signup-seller">
-      {/* GOOGLE FONTS */}
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Roboto&display=swap"
-        rel="stylesheet"
-      />
-      {/* BOOTSTRAP */}
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-        crossorigin="anonymous"
-      ></link>
-      <div class="flex-container-signupSeller">
-        <div class="flex-item-left-seller">
-          {/* <div className="welcome"> */}
-          <h2 className="welcome-text-seller"> Welcome to</h2>
-          <h1 className="name-seller">EcoVille</h1>
-          {/* </div> */}
-        </div>
+    <center>
+      <div className="signup-seller">
+        {/* GOOGLE FONTS */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Roboto&display=swap"
+          rel="stylesheet"
+        />
+        {/* BOOTSTRAP */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+          crossorigin="anonymous"
+        ></link>
+        <div class="flex-container-signupSeller">
+          {/* SIGNUP SECTION */}
 
-        {/* SIGNUP SECTION */}
+          <div class="flex-item-right-signupSeller">
+            <div className="content">
 
-        <div class="flex-item-right-signupSeller">
-          <div className="content">
-            
-            {/* SIGNUP HERE DIV */}
+              {/* SIGNUP HERE DIV */}
 
-            <div className="signup-here">
-              <h3>
-                <strong>Signup here</strong>
-              </h3>
-              <form onSubmit={(e) => onSubmit(e)}>
-                <div class="form-floating mb-3 ">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="name"
-                    placeholder="Name"
-                    name="name"
-                    value={name}
-                    onChange={(e) => onChange(e)}
-                  />
-                  <label for="name">
-                    Name
+              <div className="signup-here">
+                <h3>
+                  <strong>Signup here</strong>
+                </h3>
+                <form onSubmit={(e) => onSubmit(e)}>
+                  <div class="form-floating mb-3 ">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="name"
+                      placeholder="Name"
+                      name="name"
+                      value={name}
+                      onChange={(e) => onChange(e)}
+                    />
+                    <label for="name">
+                      Name
                     <Req />
-                  </label>
-                </div>
-                <div class="form-floating mb-3">
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="email"
-                    placeholder="name@example.com"
-                    name="email"
-                    value={email}
-                    onChange={(e) => onChange(e)}
-                    required
-                  />
-                  <label for="email">
-                    Email address
+                    </label>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="email"
+                      placeholder="name@example.com"
+                      name="email"
+                      value={email}
+                      onChange={(e) => onChange(e)}
+                      required
+                    />
+                    <label for="email">
+                      Email address
                     <Req />
-                  </label>
-                </div>
-                <div class="form-floating mb-3">
-                  <input
-                    type="password"
-                    class="form-control pass"
-                    id="password"
-                    placeholder="Password"
-                    name="password"
-                    value={password}
-                    onChange={(e) => onChange(e)}
-                    required
-                  />
-                  <label for="password">
-                    Password
+                    </label>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input
+                      type="password"
+                      class="form-control pass"
+                      id="password"
+                      placeholder="Password"
+                      name="password"
+                      value={password}
+                      onChange={(e) => onChange(e)}
+                      required
+                    />
+                    <label for="password">
+                      Password
                     <Req />
-                  </label>
-                </div>
-                <div class="form-floating mb-3">
-                  <input
-                    type="password"
-                    class="form-control pass"
-                    id="Password2"
-                    placeholder="Confirm Password"
-                    name="password2"
-                    value={password2}
-                    onChange={(e) => onChange(e)}
-                    required
-                  />
-                  <label for="password2">
-                    Confirm Password
+                    </label>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input
+                      type="password"
+                      class="form-control pass"
+                      id="Password2"
+                      placeholder="Confirm Password"
+                      name="password2"
+                      value={password2}
+                      onChange={(e) => onChange(e)}
+                      required
+                    />
+                    <label for="password2">
+                      Confirm Password
                     <Req />
-                  </label>
-                </div>
-                <div class="form-floating mb-3">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="Contact"
-                    placeholder="Phone no."
-                    name="phone"
-                    value={phone}
-                    onChange={(e) => onChange(e)}
-                  />
-                  <label for="Contact">Phone No. <Req /></label>
-                </div>
-                <div class="form-floating mb-3">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="firstline"
-                    placeholder="First Line"
-                    name="firstline"
-                    value={firstline}
-                    onChange={(e) => onChange(e)}
-                  />
-                  <label for="firstline">
-                    Address
+                    </label>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="Contact"
+                      placeholder="Phone no."
+                      name="phone"
+                      value={phone}
+                      onChange={(e) => onChange(e)}
+                    />
+                    <label for="Contact">Phone No. <Req /></label>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="firstline"
+                      placeholder="First Line"
+                      name="firstline"
+                      value={firstline}
+                      onChange={(e) => onChange(e)}
+                    />
+                    <label for="firstline">
+                      Address
                     <Req />
-                  </label>
-                </div>
-                <div class="form-floating mb-3">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="pincode"
-                    placeholder="Pincode"
-                    name="pincode"
-                    value={pincode}
-                    onChange={(e) => onChange(e)}
-                  />
-                  <label for="pincode">
-                    Pincode
+                    </label>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="pincode"
+                      placeholder="Pincode"
+                      name="pincode"
+                      value={pincode}
+                      onChange={(e) => onChange(e)}
+                    />
+                    <label for="pincode">
+                      Pincode
                     <Req />
-                  </label>
-                </div>
-                <div class="form-floating mb-3">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="landmark"
-                    placeholder="landmark"
-                    name="landmark"
-                    value={landmark}
-                    onChange={(e) => onChange(e)}
-                  />
-                  <label for="landmark">landmark <Req /></label>
-                </div>
-                <div className="tnc">
-                  <input
-                    type="checkbox"
-                    id="TnC"
-                    required
-                    onChange={activate}
-                  />
-                  <label for="TnC">
-                    {" "}
+                    </label>
+                  </div>
+                  <div class="form-floating mb-3">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="landmark"
+                      placeholder="landmark"
+                      name="landmark"
+                      value={landmark}
+                      onChange={(e) => onChange(e)}
+                    />
+                    <label for="landmark">landmark <Req /></label>
+                  </div>
+                  <div className="tnc">
+                    <input
+                      type="checkbox"
+                      id="TnC"
+                      required
+                      onChange={activate}
+                    />
+                    <label for="TnC">
+                      {" "}
                     I agree to the{" "}
-                    <a href="/tnc" alt="terms and conditions" className="tnc">
-                      terms and conditions and the privacy policy
+                      <a href="/tnc" alt="terms and conditions" className="tnc">
+                        terms and conditions and the privacy policy
                     </a>
-                  </label>
-                </div>
-                {/* <button onClick={() => getArea(pincode)}>Get Area List</button>
+                    </label>
+                  </div>
+                  {/* <button onClick={() => getArea(pincode)}>Get Area List</button>
                   <div class="form-floating mb-3">
                            <select id="cityList" name="city" value={city} onChange={(e) => onChange(e)}>
                             {postOffices.map(postOffice => {
@@ -243,19 +237,20 @@ const Register = ({ setAlert, registerSeller, isAuthenticated }) => {
                           </select>
                           <label for="cityList">Name Of Area</label>
                   </div> */}
-                
-                <input
-                  type="submit"
-                  className="btn btn-warning btn-lg"
-                  value="Register"
-                  disabled={disabled}
-                />
-              </form>
+
+                  <input
+                    type="submit"
+                    className="btn btn-warning btn-lg"
+                    value="Register"
+                    disabled={disabled}
+                  />
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </center>
   );
 };
 
