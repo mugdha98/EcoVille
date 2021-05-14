998e4f98c0ed505ddf1c7809a6eed84385bc6de0
neharewari <neharewari3@gmail.com>
2021-05-14 14:28:39 +0530

    how it works

--- client/src/components/Workflow/Workflow.js
+++ client/src/components/Workflow/Workflow.js
@@ -1,17 +1,36 @@
 import React from "react";
 import { Jumbotron } from "reactstrap";
+import bg from './bg.jpg';
+import {
+  Card,
+} from "reactstrap";
+import Image from "react-bootstrap/Image";
+const customstyle={
+  paddingLeft:"35%",
+  backgroundPosition: 'center',
+  backgroundRepeat: 'norepeat',
+  backgroundSize: 'cover',
+  height: '800px',
+}
 
 const Workflow = () => {
   return (
+    // <div>
+    //   <Jumbotron className="jumbobg">
+    //     <div></div>
+    //   </Jumbotron>
+    //   <hr />
+    // </div>
+
     <div>
-      <br />
-      <br />
-      <hr />
-      <Jumbotron className="jumbobg">
-        <div></div>
-      </Jumbotron>
-      <hr />
+      <div >
+         <img src={bg} style={customstyle}>
+         </img>
+      </div>
     </div>
+      
+     
+  
   );
 };
 
